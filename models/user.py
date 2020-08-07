0187f88a651c67df761e0d9545a30753d141f7b9
Ben Chavez <ben.m.chavez@gmail.com>
2020-08-07 18:29:14 -0500

    pull

--- models/user.py
+++ models/user.py
@@ -1,12 +1,9 @@
 import sqlite3
 from db import db
 
-
 class UserModel(db.Model):
-    # Tell SQLAlchemy the table name where these models will be stored
     __tablename__ = 'users'
 
-    # Tell SQLAlchemy what columns the table should contain
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
@@ -19,8 +16,7 @@ class UserModel(db.Model):
         db.session.add(self)
         db.session.commit()
 
-    @classmethod    # Allows you too change 'Self' & 'User to 'cls'
-    # def find_by_username(self, username):
+    @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
 
